ba91675e62432c79ce814ed7adb4fd8b5020a867
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-02-20 00:14:48 -0500

    feat: basic routing index and about page

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Index() {
+  return (
+    <div>
+      <p>Hello Next.js</p>
+      <Link href="/about">
+        <a title="About Page">About</a>
+      </Link>
+    </div>
+  );
+}
