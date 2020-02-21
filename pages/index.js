71328abc6a865fdc20cecb91605ee82278c6e1c8
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-02-20 19:58:25 -0500

    feat: add layout and post component
    Give each page a consistent look by using a layout component.
    
    Create a shell for a simple blog using the post component.

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
-import Header from '../components/Header';
+import Layout from '../components/MyLayout';
+import Link from 'next/link';
+
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
 
 export default function Index() {
   return (
     <div>
-      <Header />
-      <p>Hello Next.js!</p>
+      <Layout>
+        <h1>My Blog</h1>
+        <ul>
+          <PostLink title="Hello Next.js" />
+          <PostLink title="Learning Next.js is awesome" />
+          <PostLink title="Deploy apps with Zeit Now" />
+        </ul>
+      </Layout>
     </div>
   );
 }
