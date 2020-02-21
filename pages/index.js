48ce56881e423b4b86adb49dfa9a0c7b71f12298
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-02-20 22:11:47 -0500

    feat: add styles

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,73 @@
 import Layout from '../components/MyLayout';
 import Link from 'next/link';
 
+function getPosts() {
+  return [
+    { id: 'hello-nextjs', title: 'Hello, Next.js' },
+    { id: 'learn-nextjs', title: 'Learn that Next.js is awesome' },
+    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' },
+  ];
+}
+
 const PostLink = props => (
-  <li>
-    <Link href="/posts/[id]" as={`/posts/${props.id}`}>
-      <a>{props.title}</a>
-    </Link>
-  </li>
+  <>
+    <li>
+      <Link href="/posts/[id]" as={`/posts/${props.id}`}>
+        <a>{props.title}</a>
+      </Link>
+    </li>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        font-family: 'Arial';
+        text-decoration: none;
+        color: deepskyblue;
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+  </>
 );
 
 export default function Blog() {
   return (
-    <div>
-      <Layout>
-        <h1>My Blog</h1>
-        <ul>
-          <PostLink id="hello-nextjs" title="Hello Next.js" />
-          <PostLink id="learn-nextjs" title="Learn Next.js" />
-          <PostLink id="deploy-nextjs" title="Deploy apps with Zeit Now" />
-        </ul>
-      </Layout>
-    </div>
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        {getPosts().map(post => (
+          <PostLink key={post.id} id={post.id} title={post.title} />
+        ))}
+      </ul>
+      <style jsx>{`
+        h1,
+        a {
+          font-family: 'Arial';
+        }
+
+        ul {
+          padding: 0;
+        }
+
+        li {
+          list-style: none;
+          margin: 5px 0;
+        }
+
+        a {
+          text-decoration: none;
+          color: deepskyblue;
+        }
+
+        a:hover {
+          opacity: 0.6;
+        }
+      `}</style>
+    </Layout>
   );
 }
