bfba7fa635a8272c257c3a8a19518fd18b4a9968
Jack Kinsey <jack.w.kinsey@gmail.com>
2020-02-20 19:27:40 -0500

    feat: add Header component
    This adds a header component that contains links to the index and about
    pages of the app. The header is then imported into each page and used as
    a kind of navigation for the app.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
-import Link from 'next/link';
+import Header from '../components/Header';
 
 export default function Index() {
   return (
     <div>
-      <p>Hello Next.js</p>
-      <Link href="/about">
-        <a title="About Page">About</a>
-      </Link>
+      <Header />
+      <p>Hello Next.js!</p>
     </div>
   );
 }
